81f8d3effc93dc9e2a04238ca781f93be112c2dd
robertreingit <gerrobrein@yahoo.com.au>
2014-06-30 14:28:12 +0200

    Added some more testing.

--- QuaternionTest.m
+++ QuaternionTest.m
@@ -16,6 +16,8 @@ classdef QuaternionTest < matlab.unittest.TestCase
             testCase.verifyEqual(q1-q2,Quaternion(p1 - p2));
             testCase.verifyEqual(s * q1,Quaternion(s * p1));
             testCase.verifyEqual(q1 * s,Quaternion(s * p1));
+            testCase.verifyEqual(q1.norm(), norm(p1));
+            testCase.verifyTrue(norm(q1 * q1.inv() - Quaternion.Identity) < eps);
             
         end % testOperators
         
